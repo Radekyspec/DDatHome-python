e3870f16b5d2736e1376c7f6dcae28fe7a73e909
Radekyspec <93813543+Radekyspec@users.noreply.github.com>
2023-05-22 17:09:31 -0400

    [1.1.3] changelog

--- connector.py
+++ connector.py
@@ -14,7 +14,7 @@ from logger import Logger
 
 
 class Connector:
-    VERSION: str = "1.1.2"
+    VERSION: str = "1.1.3"
     DEFAULT_INTERVAL: int = 1000
     DEFAULT_SIZE: int = 10
     DEFAULT_LIMIT: int = 1000
