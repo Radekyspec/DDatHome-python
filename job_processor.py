98a4cb649ca9c92e3cd5bb61249b22d3b3da79ac
Radekyspec <93813543+Radekyspec@users.noreply.github.com>
2023-05-22 17:06:26 -0400

    [1.2.3] add wbi api support

--- job_processor.py
+++ job_processor.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import hashlib
 import json
 import queue
 import time
@@ -9,7 +10,9 @@ from typing import Any
 from aiohttp import ClientSession
 from aiohttp.client_exceptions import ClientError
 from async_timeout import timeout
+from functools import reduce
 from random import random
+from urllib.parse import urlencode, urlsplit, parse_qsl
 
 from logger import Logger
 from ws_live import WSLive
@@ -32,7 +35,7 @@ class JobProcessor:
         self.websockets = None
         self.queue: queue.PriorityQueue = queue.PriorityQueue()
         self.logger: Any = Logger(
-            logger_name="job").get_logger()
+            logger_name="job", level="DEBUG").get_logger()
         self.closed: bool = False
         self.client: Any = None
         self.bili_ws = WSLive(self.WS_LIMIT)
@@ -78,6 +81,33 @@ class JobProcessor:
         async with client.get(url) as resp:
             return await resp.text(encoding="utf-8")
 
+    @staticmethod
+    def get_mixin_key(ae):
+        oe = [46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42, 19, 29, 28, 14, 39,
+              12,
+              38, 41,
+              13, 37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+              36,
+              20, 34, 44, 52]
+        le = reduce(lambda s, i: s + ae[i], oe, "")
+        return le[:32]
+
+    async def enc_wbi(self, params: dict):
+        async with self.client.get("https://api.bilibili.com/x/web-interface/nav") as resp:
+            resp = json.loads(await resp.text(encoding="utf-8"))
+        wbi_img: dict = resp["data"]["wbi_img"]
+        img_url: str = wbi_img.get("img_url")
+        sub_url: str = wbi_img.get("sub_url")
+        img_value = img_url.split("/")[-1].split(".")[0]
+        sub_value = sub_url.split("/")[-1].split(".")[0]
+        me = self.get_mixin_key(img_value + sub_value)
+        wts = int(time.time())
+        params["wts"] = wts
+        params = {new_key: params[new_key] for new_key in sorted(params.keys())}
+        query = urlencode(query=params, encoding="utf-8")
+        w_rid = hashlib.md5((query + me).encode(encoding='utf-8')).hexdigest()
+        return w_rid, wts
+
     async def process(self):
         """Process http task and send back to server
         """
@@ -91,6 +121,17 @@ class JobProcessor:
                 try:
                     with timeout(10):
                         # resp = await self.fetch(client, url)
+                        url_split = urlsplit(url)
+                        if "wbi" in str(url_split.path):
+                            query = dict(parse_qsl(urlsplit(url).query))
+                            query = {new_key: query[new_key] for new_key in sorted(query.keys())}
+                            w_rid, wts = await self.enc_wbi(query.copy())
+                            query.update({
+                                "w_rid": w_rid,
+                                "wts": wts
+                            })
+                            url = "".join([url.split("?")[0], "?", urlencode(query, encoding="utf-8")])
+                            self.logger.debug(f"New url: {url}")
                         resp: asyncio.Task = asyncio.create_task(
                             self.fetch(self.client, url))
                         resp: str = await resp
